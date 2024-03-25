fd0214b3037743680f51a9973bdf5b635e4a71ba
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-25 07:16:48 +0530

    added Banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Banner.css'
+const Banner = () => {
+    return (
+        <div className='col'>
+            <div className='row text-center head'>
+               <p className='fw-bolder shop'>Shop in Style</p>
+               <p className="lead fw-normal text-white-50 ">With this shop hompeage template</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
