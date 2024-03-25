62765f1486b6e94a90b140559f87410d3c28cea6
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-25 07:29:06 +0530

    added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="card">
+                <div className="card-body">
+                    <p className="card-title text-center">Copyright © Your Website 2023</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
