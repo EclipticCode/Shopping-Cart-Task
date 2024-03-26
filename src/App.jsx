c5e04b93a5a2d124773d337956355949fb2a55f8
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-26 12:49:09 +0530

    added airpods img

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
     setCount(count - 1)
   }
    const cardDatas = [
-    { id: 1, img: img, title: "Fancy Product", price: "$40.00-$80.00" },
+    { id: 1, img: "https://www.imore.com/sites/imore.com/files/styles/small/public/field/image/2019/10/airpods-pro-render.png", title: "Fancy Product", price: "$40.00-$80.00" },
     { id: 2, img: "./src/assets/cardimage.jpg", title: "Special Item", price: "$18.00" },
     { id: 3, img: "./src/assets/cardimage.jpg", title: "Sale Item", price: "$25.00" },
     { id: 4, img: "./src/assets/cardimage.jpg", title: "Popular Item", price: "$40.00" },
